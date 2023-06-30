303299756414013b9040c333a081fb7ab451b828
dychou <86593151+dychou@users.noreply.github.com>
2023-06-30 09:32:13 +0900

    Add files via upload

---
+++ 2. DQL语言详解.sql
@@ -0,0 +1,73 @@
+-- 无条件查询(全查询):检索表中所有的数据
+-- 语法结构:select 列名, 列名, 列名 from 表名;
+-- 需求:查询stu表中所有学生的学号、姓名、年龄
+select no, name, age from stu;
+
+-- (1). 查询时我们可以对列中的数据进行四则运算
+-- 需求: 查询stu表中所有学生的姓名和成绩(成绩+10分处理)
+select name, score + 10 from stu;
+-- 注意:null无法进行任何操作,任何对null值的处理得到的结果都是null
+
+-- (2). 查询时我们还可以对列中的数据进行拼接的操作
+-- 语法结构:concat(参数, 参数, 参数),将括号内的参数进行拼接
+-- 需求:查询stu表中所有学生的学号、姓名、成绩(姓名后面拼接'是个好人')
+select no, concat(name, '是个好人'), score from stu;
+-- 注意:加号在MySQL中只表示求和,如果加号两端遇到文字,直接将文字当做0处理
+
+-- (3). 在不修改查询结果的前提下, 可以将列名进行修改(列名起别名)
+-- 语法结构:select 列名 as 别名, 列名 as 别名 from 表名;
+-- 拓展:as可以省略
+
+select no, concat(name, '是个好人') name, score from stu;
+-- 注意:别名不需要添加单引号,因为别名不是数据,是列名
+
+-- 有条件查询(过滤查询):检索表中满足条件的数据
+-- 语法结构: select 列名, 列名, 列名 from stu where 条件
+-- 需求:查询stu表中所有成年学生的学号,姓名和年龄
+select no, name, age from stu where age >= 18;
+
+-- (1). between .. and ..用法:判断某个区间
+-- 语法结构: where 列名 between a and b, 判断列中的数据是否在a~b的范围之内
+-- 语法结构: where 列名 not between a and b, 判断列中的数据是否不在a~b的范围之内
+-- 需求:查询stu表中成绩在60-80之间的学生学号、姓名和成绩
+select no, name, score from stu where score >= 60 and score <= 80;
+select no, name, score from stu where score between 60 and 80;
+select no, name, score from stu where score not between 60 and 80;
+
+-- (2). in(..)用法:判断某个值
+-- 语法结构:where 列名 in(a, b, c),表示判断列中数据是否等于a或者等于b或者等于c
+-- 语法结构:where 列名 not in(a, b, c),表示判断列中数据是否不等于a或者不等于b或者不等于c
+-- 需求:查询1班或者3班的所有学生学号、姓名和班级
+select no, name, class from stu where class = 1 or class = 3;
+select no, name, class from stu where class in(1, 3);
+
+-- (3). null值的判断
+-- 语法结构: where 列名 is null,判断该列数据为null
+-- 语法结构: where 列名 is not null,判断该列数据不为null
+select no, name, class from stu where class is not null;
+
+-- 排序查询:对查询结果进行排序处理
+-- 语法结构:order by 列名 asc/desc
+-- asc: ascending, 升序
+-- desc: descending, 降序
+-- 需求:查询stu表中所有学生的姓名和成绩,并且按照成绩降序排序
+select name, score from stu order by score desc;
+-- 拓展:如果需要对数据进行升序排序,可以写asc或者直接省略(默认升序)
+
+-- 模糊查询(关键字查询):根据某个关键字匹配数据
+-- 语法结构: where 列名 like '匹配规则',判断列中是否包含指定关键字
+-- 语法结构: where 列名 not like '匹配规则',判断列中是否不包含指定关键字
+
+-- 匹配规则中有两种匹配符号分别为:
+-- _ : 匹配一个长度的任意文字/数字/符号
+-- % : 匹配任意长度(包含0个长度)的任意文字/数字/符号
+
+-- 需求: 查询stu表中名字包含'老'字的学生学号、姓名和年龄
+select no, name, age from stu where name not like '%老%';
+
+
+
+
+
+
+
---
+++ 3. 单行函数详解.sql
@@ -0,0 +1,97 @@
+-- MySQL函数:类似于Java的方法可以处理数据
+-- 根据处理数据的特点不同,函数共分为两种:
+-- 1. 单行函数:每一条数据经过单行函数处理后返回一行结果
+-- 2. 聚合函数:多条数据经过聚合函数处理后只返回一行结果
+
+-- 单行函数:根据特点,单行函数又分为四小种
+-- 注意:函数既可以当做查询的列来使用又可以当做判断条件使用
+
+-- (1). 字符型单行函数:用来处理字符类型的数据
+-- 拓展:MySQL中对数据类型不敏感,1和'1'是等价的
+-- 1>. length(str):求参数数据的字节长度
+-- 需求:查询student表中所有学生姓名以及姓名的字节长度
+-- 字节长度:中文一个文字占3个长度,英文一个字母占一个字节长度
+select name, length(name) from student;
+
+select length('james');
+
+-- 2>. concat(str1, str2, str3, str4):将参数进行拼接处理
+
+-- 3>. substring(str, begin, length):将指定数据从指定位置截取指定长度
+select substring('我爱你中国', 2, 3);
+
+-- 需求:查询emp表中所有员工的工号、姓名、入职年份
+select no, name, substring(joindate, 1, 4) from emp;
+
+select substring('我爱你中国', 2);
+
+-- 4>. replace(str, old, new):替换字符串数据
+select replace('abcdcfch', 'cdc', 'm');
+
+-- 5>. instr(str, value):返回第二个参数在第一个参数中首次出现的位置
+select instr('abcdcfch', 'q');
+-- 拓展:如果不存在则返回0
+
+-- 6>. upper(str)/lower(str):将参数数据全大写/全小写处理
+select upper('abcdefgh');
+select lower('ABCDEFGH');
+
+-- (2). 数字型单行函数:用来处理数字类型的数据
+-- 1>. round(num):对参数进行四舍五入的操作
+select round(123.656);
+
+-- 2>. round(num, positon):对参数数据指定位置进行四舍五入的操作
+select round(123.456, -1);
+
+-- 3>. mod(num1, num2):求两个参数取余数(取模)的结果
+select mod(10, 4);
+
+-- (3). 日期型单行函数:只能处理日期格式的数据
+-- 1>. year(date):提取参数的年份
+select year('20221012');
+
+-- 2>. month(date):提取参数的月份
+select month(joindate) from emp;
+
+-- 3>. day(date):提取参数的日期
+
+
+-- (4). 功能型单行函数
+-- 1>. ifnull(str, result):如果第一个参数为null,则将结果替换为第二个参数(☆☆☆☆☆)
+
+-- 需求:查询student表中所有学生的姓名和班级(如果班级为空,则用未知表示)
+select name, ifnull(class, '未知') from student;
+
+-- 2>. if(condition, result1, result2):如果参数一的条件成立,则结果为第二个参数,否则结果为第三个参数
+-- 需求:查询student表中所有学生的姓名和成绩(只显示及格或者不及格)
+select name, if(score >= 60, '及格', '不及格') from student;
+
+-- 3>. case .. when .. then .. else .. end ..语法:
+-- 需求:查询emp表中所有员工的姓名,工资,职位
+-- 要求:职位是'CEO',显示'老板',职位'Project Manager',显示'项目经理',否则显示'普通员工'
+
+-- 写法一:只能判断相等的情况
+select name, salary, 
+case job
+when 'CEO' then '老板'
+when 'Project Manager' then '项目经理'
+else '普通员工'
+end
+from emp;
+
+-- 写法二:相等和不等都可以进行判断
+select name, salary, 
+case
+when job = 'CEO' then '老板'
+when job = 'Project Manager' then '项目经理'
+else '普通员工'
+end
+from emp;
+
+
+
+
+
+
+
+
